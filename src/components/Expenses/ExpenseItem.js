67c1b8faceb66d9144356c1356f50866ab5ffead
Yash Pandya <yash.pandya@briskteq.com>
2023-03-14 12:08:51 +0530

    structure maintain

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,11 @@
-import Card from "./Card";
-import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import ExpenseDate from "../Expenses/ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  // const expenseDate = new Date(2023, 3, 14);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 233;
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
 
   return (
     <Card className="expense-item">
@@ -14,8 +14,9 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,10 @@
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className;
-  console.log(classes);
+  // console.log(classes);
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
 
